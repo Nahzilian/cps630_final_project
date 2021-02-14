136a6a352a488cfdc8f6bb07e7e8ae6fe19e489b
sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2021-02-14 02:20:03 -0500

    footer created

--- template/footer.php
+++ template/footer.php
@@ -19,4 +19,18 @@
   });
   </script>
   </body>
+  <footer class="z-depth-5">
+    <div class="pink accent-2">
+      <div class="copyright" align="center">
+        <script>
+          document.write('&copy;' );
+          document.write(' 2021 - ');
+          document.write(new Date().getFullYear());
+          document.write(' www.devimayair.com - All Rights Reserved.');
+          document.write('<br/>Last Updated : ');
+          document.write(document.lastModified);
+        </script>
+      </div>
+    </div>
+  </footer>
 </html>
