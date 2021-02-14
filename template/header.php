87f312217d809861412570cef2705c360dd2e436
sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2021-02-13 22:56:07 -0500

    materialize added

--- template/header.php
+++ template/header.php
@@ -3,6 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>Devil May Air</title>
-    <link rel="stylesheet" href="./">
+    <link rel="stylesheet" href="./res/css/index.css">
+    <link rel="stylesheet" href="./res/css/materialize/css/materialize.min.css">
+    <script type="text/javascript" src="./res/css/materialize/js/materialize.min.js"></script>
   </head>
+
   <body>
