79de21ba16124beaad7384dedd2e0473222f7c77
Duc Nguyen <nahz3112@gmail.com>
2021-02-19 21:30:58 -0500

    Drag drop fixed

--- service.php
+++ service.php
@@ -15,11 +15,9 @@ while ($flower = $flowers->fetch_assoc()) {
 }
 ?>
 
+<div class = "mycart" id = "mycart"><i class="fas fa-shopping-cart"></i></div>
 
 <?php if (isset($_GET['type']) && $_GET['type'] == 'driver') : ?>
-  
-  <div class = "mycart" ondrop="drop(event)" ondragover="allowDrop(event)"><i class="fas fa-shopping-cart"></i></div>
-
   <h1>Driver</h1>
   <div class="row service">
     <table class="striped">
@@ -37,16 +35,16 @@ while ($flower = $flowers->fetch_assoc()) {
         <?php foreach ($allCar as $car) : ?>
           <tr>
             <td class="first">
-              <article id=<?php echo "drag". $car['CAR_ID'] ?> draggable="true" ondragstart="drag(event)" class="fas fa-bars">
+              <article id=<?php echo "drag-car-". $car['CAR_ID'] ?> draggable="true" class="fas fa-bars draggable">
               </article> <img class="materialboxed" width="100" height="100" src=<?php echo "./res/img/car/car" . strval(intval($car['CAR_ID']) % 6 + 1) . ".jpeg" ?> alt="car-image"><?php echo $car['CAR_MODEL'] ?>
             </td>
             <td>
               <div class="input-field col s12  m8 l8">
-                <input type="text" id="source" name="source" value="">
+                <input type="text" id=<?php echo "source-". $car['CAR_ID'] ?> name="source" value="">
                 <label for="source">Source</label>
               </div>
               <div class="input-field col s12  m8 l8">
-                <input type="text" id="destin" name="destin" value="">
+                <input type="text" id=<?php echo "destin-". $car['CAR_ID'] ?> name="destin" value="">
                 <label for="destin">Destination</label>
               </div>
               <button class="btn-large col s12 m6 l6" type="button" name="show">Show Map</button>
@@ -76,7 +74,7 @@ while ($flower = $flowers->fetch_assoc()) {
         <?php foreach ($allFlower as $flower) : ?>
           <tr>
             <td class="first">
-              <article ondrag="onDrag(event)" draggable="true" class="fas fa-bars">
+              <article id=<?php echo "drag-flower-". $flower['CAR_ID'] ?> draggable="true" class="fas fa-bars draggable">
               </article> <img class="materialboxed" width="100" height="100" src=<?php echo "./res/store/flower" . strval(intval($flower['FLOWER_ID']) % 15 + 1) . ".jpeg" ?> alt="flower-image"><?php echo $flower['STORE_CODE'] ?>
             </td>
             <td>
@@ -100,7 +98,6 @@ while ($flower = $flowers->fetch_assoc()) {
   </div>
 <?php endif; ?>
 
-
 <!-- <div class="" id="map"> -->
 
 <!-- </div> -->
@@ -109,28 +106,34 @@ while ($flower = $flowers->fetch_assoc()) {
 <?php include './template/contact_about.php' ?>
 <?php include './template/footer.php' ?>
 <script type="text/javascript">
-  // function onDrag(event) {
-  //   // console.log(event.dat);
+  var countItem = 0;
+  var listOfOrder = [];
+  $(document).ready(function() {
+	$('.draggable').on('dragstart', function(e){
+     var source_id = $(this).attr('id');
+     console.log(source_id);
+	   e.originalEvent.dataTransfer.setData("source_id", source_id); 
+	});
+	
+	$("#mycart").on('dragenter', function (e){
+		e.preventDefault();
+		$(this).css('background', '#BBD5B8');
+	});
 
-  // }
+	$("#mycart").on('dragover', function (e){
+		e.preventDefault();
+	});
 
-  // function drop(event) {
-  //   event.preventDefault();
-  //   var data = event.dataTransfer.getData("Text");
-  //   event.target.appendChild(document.getElementById(data))
-  // }
-  function allowDrop(ev) {
-    ev.preventDefault();
-  }
+	$("#mycart").on('drop', function (e){
+		e.preventDefault();
+    var product_code = e.originalEvent.dataTransfer.getData('source_id');
+    console.log(product_code);
+    console.log('dropped')
+    countItem++;
+    $("#mycart").html(`<div class = "mycart" id = "mycart"><i class="fas fa-shopping-cart"></i> ${countItem}</div>` );
+  });
 
-  function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
-
-  function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
+  
+});
   
 </script>
