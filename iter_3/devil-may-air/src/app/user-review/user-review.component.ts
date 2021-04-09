829e0a73de20f74ee782ee12b1c91dc48f7156d0
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2021-04-09 13:02:58 -0400

    UPDATE: init design for selecting drivers to review

--- iter_3/devil-may-air/src/app/user-review/user-review.component.ts
+++ iter_3/devil-may-air/src/app/user-review/user-review.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { getAllCar } from '../../utils/api/publicAPI';
 
+import Car from '../../models/car';
+
 @Component({
   selector: 'app-user-review',
   templateUrl: './user-review.component.html',
@@ -8,8 +10,22 @@ import { getAllCar } from '../../utils/api/publicAPI';
 })
 export class UserReviewComponent implements OnInit {
   select_img_size: number;
+  allCar: Array<Car>;
+
   constructor() {
     this.select_img_size = 35;
+    this.getCars();
+  }
+
+  async getCars(){
+    let tempCar = await getAllCar(0, 100);
+    this.allCar = tempCar.data.data;
+  }
+
+  getCarImgSrc(id) {
+    const max = 5
+    if (id <= max) return `../../../assets/img/car/car${id}.jpeg`;
+    return '../../../assets/img/car/plc.jpeg';
   }
 
   ngOnInit(): void {
