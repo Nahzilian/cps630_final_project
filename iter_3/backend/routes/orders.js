4bbf26471b3b94198bab39349c573baa29287030
Duc Nguyen <nahz3112@gmail.com>
2021-04-10 17:51:35 -0400

    UPDATE: Added get for customer order

--- iter_3/backend/routes/orders.js
+++ iter_3/backend/routes/orders.js
@@ -3,7 +3,7 @@ const Car = require('../models/Car');
 const Trip = require('../models/Trip');
 const User = require('../models/User');
 const CustomerOrder = require('../models/CustomerOrder');
-const {tripValidation, orderValidation} = require('../utils/validation');
+const { tripValidation, orderValidation } = require('../utils/validation');
 const { userInfoFormat } = require('../utils/formatting');
 
 const { validateToken } = require('../utils/authentication');
@@ -17,11 +17,11 @@ router.post('/', validateToken, async (req, res, next) => {
         price: req.body.price
     }
     const serviceCar = await Car.findById(req.body.carId);
-    if(!serviceCar) return res.status(400).send({msg: "Driver is not available"});
+    if (!serviceCar) return res.status(400).send({ msg: "Driver is not available" });
 
     const newTripValidation = tripValidation(newTripObj);
-    if(newTripValidation.error) return res.status(400).send(newTripValidation.error.details[0].message);
-    
+    if (newTripValidation.error) return res.status(400).send(newTripValidation.error.details[0].message);
+
     const newTrip = new Trip(newTripObj);
     const savedTrip = await newTrip.save();
 
@@ -29,29 +29,46 @@ router.post('/', validateToken, async (req, res, next) => {
         totalPrice: req.body.totalPrice,
         customerId: req.body.customerId,
         flowers: req.body.flowers,
-        tripId:  savedTrip._id.toString()
+        tripId: savedTrip._id.toString()
     }
     const buyer = await User.findById(req.body.customerId);
 
-    if(!buyer) return res.status(400).send({msg: "User account is not available"});
+    if (!buyer) return res.status(400).send({ msg: "User account is not available" });
     const remainBalance = parseFloat((buyer.balance - req.body.totalPrice).toFixed(2));
 
-    if(remainBalance < 0) return res.status(400).send({msg: "Insufficient balance"});
-    const updateUser = await User.updateOne({_id: buyer._id}, {balance: remainBalance});
-    
+    if (remainBalance < 0) return res.status(400).send({ msg: "Insufficient balance" });
+    const updateUser = await User.updateOne({ _id: buyer._id }, { balance: remainBalance });
+
     const newOrderValidation = orderValidation(newOrderObj);
-    if(newOrderValidation.error) return res.status(400).send(newOrderValidation.error.details[0].message);
+    if (newOrderValidation.error) return res.status(400).send(newOrderValidation.error.details[0].message);
 
     const updatedUser = await User.findById(req.body.customerId);
     const newOrder = new CustomerOrder(newOrderObj);
     const savedOrder = await newOrder.save();
     console.log(userInfoFormat(updatedUser))
-    return res.status(200).send({msg: "Added successfully",user: userInfoFormat(updatedUser)});
+    return res.status(200).send({ msg: "Added successfully", user: userInfoFormat(updatedUser) });
 })
 
-router.get('/', (req, res, next) => {
+router.get('/:id', validateToken, async (req, res, next) => {
+    const orderId = req.params.id;
+    const customerId = req.body.customerId;
+    const isAdmin = req.body.isAdmin;
+
+    if (orderId) {
+        const customerOrdById = await CustomerOrder.findById(orderId);
+        return res.json(customerOrdById);
+    }
+    if (isAdmin) {
+        const allOrderAdmin = await CustomerOrder.find({});
+        return res.json(allOrderAdmin);
+    }
+
+    const allOrder = await CustomerOrder.find({ customerId: customerId });
+    return res.json(allOrder);
 
 })
+
+// Order should not be updated or delete
 
 
 module.exports = router;
