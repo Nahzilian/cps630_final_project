222c4c3da000e76418eb7412962c577d0fe95cbf
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2021-04-11 13:39:52 -0400

    UPDATE: added pagination and group button for green services

--- iter_3/devil-may-air/src/app/main-services/green/green.component.ts
+++ iter_3/devil-may-air/src/app/main-services/green/green.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 
+import { getAllCar, findReview } from '../../../utils/api/publicAPI';
+import Car from '../../../models/car';
 import { MapComponent } from '../map/map.component';
+import { FormControl } from '@angular/forms';
+import { PageEvent } from '@angular/material/paginator'; 
 
 @Component({
   selector: 'app-green',
@@ -9,8 +13,16 @@ import { MapComponent } from '../map/map.component';
 })
 export class GreenComponent implements OnInit {
   @ViewChild (MapComponent) map!: MapComponent;
-
+  allCar: Array<Car>;
   
+  length = 100;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+
+  pageEvent: PageEvent;
+
+  toggleControl = new FormControl('driver');
+
   // Google map
   source: string;
   destin: string;
@@ -18,6 +30,20 @@ export class GreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  updateData (event?: PageEvent) {
+    this.pageSize = event.pageSize;
+    this.getCars(event.pageIndex);
+    return event;
+  }
+
+  async getCars(page) {
+    let tempCar = await getAllCar(page, this.pageSize);
+    this.allCar = tempCar.data.data;
+    this.length = tempCar.data.row;
+    
+    // this.updateReview();
   }
 
   getSourceAddress(place: object) {
