95f46ad57871d57fa531f794734b4c98c8776df2
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2021-04-11 01:51:15 -0400

    UPDATE: Driver reviews added

--- iter_3/devil-may-air/src/app/main-services/driver-service/driver-service.component.ts
+++ iter_3/devil-may-air/src/app/main-services/driver-service/driver-service.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { getAllCar } from '../../../utils/api/publicAPI';
+import { getAllCar, findReview } from '../../../utils/api/publicAPI';
 import { PageEvent } from '@angular/material/paginator';
 
 import Car from '../../../models/car';
@@ -26,31 +26,70 @@ export class DriverServiceComponent implements OnInit {
   source: string;
   destin: string;
 
+  
+  //Review Content 
+  allReviews: Array<number>;
+  
+  async getReview(id){
+
+    let reviews = await findReview(id);
+    
+    return reviews;
+  }
+
+  updateReview(){
+    // Get Reviews
+    for (let i = 0; i < this.allCar.length; i++) {
+      const element = this.getReview(this.allCar[i].carCode);
+      this.allCar[i].usersVoted = 0;
+      this.allCar[i].score = 0;
+      element.then((e)=>{
+        let score = 0;
+        let totUsers = 0;
+        e.data.forEach(element => {
+          score += element.score;
+          totUsers++;
+        });
+        this.allCar[i].usersVoted = totUsers;
+        let tot = (score/totUsers) * 5;
+        this.allCar[i].score = tot < 0? 0:tot;
+        
+      });
+      
+    }
+  }
+
   updateData (event?: PageEvent) {
     this.pageSize = event.pageSize;
     this.getCars(event.pageIndex);
-  }
 
+    
+    
+  }
+  
   setPageSizeOptions(setPageSizeOptionsInput: string) {
     if (setPageSizeOptionsInput) {
       this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
     }
   }
-
+  
   constructor() {
     this.getCars(0);
+    
   }
-
+  
   async getCars(page) {
     let tempCar = await getAllCar(page, this.pageSize);
     this.allCar = tempCar.data.data;
     this.length = tempCar.data.row;
+    
+    this.updateReview();
   }
-
+  
   getSourceAddress(place: object) {
     this.source = place['formatted_address'];
   }
-
+  
   getDestinAddress(place: object) {
     this.destin = place['formatted_address'];
   }
