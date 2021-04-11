8972ae0b091b85b45ba9b99d8ad0e1510ced3bf6
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2021-04-11 11:05:33 -0400

    UPDATE: Flower service has ratings for each product

--- iter_3/devil-may-air/src/app/main-services/flower-service/flower-service.component.ts
+++ iter_3/devil-may-air/src/app/main-services/flower-service/flower-service.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewChild, OnChanges, Output, EventEmitter } from '@
 import { PageEvent } from '@angular/material/paginator';
 import Flower from 'src/models/flower';
 import { MapComponent } from '../map/map.component';
-import { getAllFlower } from '../../../utils/api/publicAPI';
+import { getAllFlower, findReview } from '../../../utils/api/publicAPI';
 import { Router } from '@angular/router';
 
 import { CartService } from '../../../utils/services/cart.service';
@@ -26,17 +26,25 @@ export class FlowerServiceComponent implements OnInit, OnChanges {
   length = 100;
   pageSize = 10;
   pageSizeOptions: number[] = [5, 10, 25, 100];
-
+  
   // MatPaginator Output
   pageEvent: PageEvent;
-
+  
   // Google map
   source: string;
   destin: string;
   error: string;
-
+  
   // Drag drop
   itemCount: number
+  
+  //Review Content 
+  allReviews: Array<number>;
+  
+  constructor(private router: Router, private cartService :CartService) {
+    this.getFlowers(0);
+    this.cart = [];
+  }
 
   drop(event: CdkDragDrop<string[]>) {
     //this.cart.push()
@@ -47,6 +55,34 @@ export class FlowerServiceComponent implements OnInit, OnChanges {
         event.currentIndex);
         this.itemCount = this.cart.length;
     })
+  }
+
+  updateReview(){
+    // Get Reviews
+    for (let i = 0; i < this.allFlower.length; i++) {
+      const element = this.getReview(this.allFlower[i].storeCode);
+      this.allFlower[i].usersVoted = 0;
+      this.allFlower[i].score = 0;
+      element.then((e)=>{
+        let score = 0;
+        let totUsers = 0;
+        e.data.forEach(element => {
+          score += element.score;
+          totUsers++;
+        });
+        this.allFlower[i].usersVoted = totUsers;
+        let tot = (score/totUsers) * 5;
+        this.allFlower[i].score = tot < 0? 0:tot;
+        
+      });
+      
+    }
+  }
+  async getReview(id){
+
+    let reviews = await findReview(id);
+    
+    return reviews;
   }
 
   async checkOut() {
@@ -72,21 +108,20 @@ export class FlowerServiceComponent implements OnInit, OnChanges {
     this.cartService.setData(obj);
     this.router.navigate(['/cart']);
   }
-
-  constructor(private router: Router, private cartService :CartService) {
-    this.getFlowers(0);
-    this.cart = [];
-  }
+  
 
   updateData(event?: PageEvent) {
     this.pageSize = event.pageSize;
     this.getFlowers(event.pageIndex);
+
   }
 
   async getFlowers(page) {
     let tempCar = await getAllFlower(page, this.pageSize);
     this.allFlower = tempCar.data.data;
     this.length = tempCar.data.row;
+
+    this.updateReview();
   }
 
   setPageSizeOptions(setPageSizeOptionsInput: string) {
