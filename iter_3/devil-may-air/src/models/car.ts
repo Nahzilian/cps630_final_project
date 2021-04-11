2ca73f4b8ea3f6b9ddf1bb5430cc9cefe20ac9ed
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2021-04-11 01:52:04 -0400

    UPDATE: change car front-end model by adding score and total users voted

--- iter_3/devil-may-air/src/models/car.ts
+++ iter_3/devil-may-air/src/models/car.ts
@@ -4,17 +4,23 @@ export default class Car {
   model: string;
   carCode: string;
   imageid: number;
+  score: number;
+  usersVoted: number;
   constructor(
     available: boolean,
     _id: string,
     model: string,
     carCode: string,
     imageid: number,
+    score: number,
+    usersVoted: number
   ) {
     this.available = available;
     this.id = _id;
     this.model = model;
     this.carCode = carCode;
     this.imageid = imageid;
+    this.score = score;
+    this.usersVoted = usersVoted;
   }
 }
