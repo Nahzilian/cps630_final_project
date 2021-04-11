0833741ca998ecf279e53c572d39c8be929291e7
Duc Nguyen <nahz3112@gmail.com>
2021-04-11 17:16:17 -0400

    UPDATE: Added "Create User" form

--- iter_3/devil-may-air/src/app/db-maintain/create-form/create-form.component.ts
+++ iter_3/devil-may-air/src/app/db-maintain/create-form/create-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { register } from 'src/utils/api/apiController';
 
 @Component({
   selector: 'app-create-form',
@@ -6,6 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-form.component.sass']
 })
 export class CreateFormComponent implements OnInit {
+  error: String;
+  @Input() formType: String;
+  userForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    address: new FormControl(''),
+    cityCode: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+    creditCard: new FormControl('')
+  });
+
+  carForm: FormGroup = new FormGroup({
+    
+  })
+
+  submitUser() {
+    if (this.userForm.valid) {
+      try {
+        register(this.userForm.controls);
+      } catch (err) {
+        this.error = err
+      }
+    }
+  }
+
 
   constructor() { }
 
